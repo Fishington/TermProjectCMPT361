80bad59662620dfd60d5616325ca157fea3ac688
Stefan Cvijovic <164532223+cvijovics@users.noreply.github.com>
2025-12-02 15:06:14 -0700

    implemented password management, and proper private/public key implementation for both client and server-side

--- client/client.py
+++ client/client.py
@@ -3,20 +3,62 @@ import socket
 import sys
 import os
 from Crypto.Cipher import AES
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
 from Crypto.Util.Padding import pad, unpad
 
 # Helper Functions For Encryption/Decryption
 
-def getKey():
-    with open('../key', 'rb') as f:
-        key = f.read()
-    return key
+#def getKey():
+#    with open('../key', 'rb') as f:
+#        key = f.read()
+#    return key
 
+def generateRSAKeys(username):
+    # Checks if keys exist on client machine
+    if os.path.exists(f'../{username}_public.pem') and os.path.exists(f'../{username}_private.pem'):
+        with open(f'../{username}_public.pem', 'rb') as f:
+            public_key = f.read()
+
+        with open(f'../{username}_private.pem', 'rb') as f:
+            private_key = f.read()
+        return public_key, private_key
+    
+    else:
+    # Generates RSA key pair and saves to client machine
+        key = RSA.generate(2048)
+        private_key = key.export_key()
+        with open(f'../{username}_private.pem', 'wb') as f:
+            f.write(private_key)
+
+        public_key = key.publickey().export_key()
+        with open(f'../{username}_public.pem', 'wb') as f:
+            f.write(public_key)
+        return public_key, private_key
+
+# Saves the server's public key for later use, if doesnt already exist
+def saveServerPubKey(key):
+    if not os.path.exists(f'../server_public.pem'):
+        with open(f'../server_public.pem', 'wb') as f:
+            f.write(key)
+
+# For sending encrypted username to server
+def encryptMessageRSA(public_key, message):
+    pubkey = RSA.import_key(public_key)
+    cipher_rsa_en = PKCS1_OAEP.new(pubkey)
+    enc_data = cipher_rsa_en.encrypt(message.encode('ascii'))
+    return enc_data
+
+# For receiving encrypted messages using privkey from client
+def decryptMessageRSA(private_key, encrypted_message):
+    privkey = RSA.import_key(private_key)
+    cipher_rsa_dec = PKCS1_OAEP.new(privkey)
+    dec_data = cipher_rsa_dec.decrypt(encrypted_message)
+    return dec_data
 
 # Same as server.py
 def encryptMessage(key, message):
     cipher = AES.new(key, AES.MODE_ECB)
-
     # Bytes then padded
     padded_message = pad(message.encode('ascii'), AES.block_size)
     encryped_message = cipher.encrypt(padded_message)
@@ -33,7 +75,7 @@ def client():
     # Server Information
     # serverName = '127.0.0.1' #'localhost'
     serverPort = 12001
-    
+    username = ""
     #Create client socket that useing IPv4 and TCP protocols 
     try:
         clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -42,137 +84,153 @@ def client():
         sys.exit(1)    
     
     try:
+        # Prior to recieving symkey, use RSA to exchange username/password
+        # Encrpyt using server pubkey when sending to server, decrypt using client privkey when receiving from server
+
         #Client connect with the server
         serverName = input("Enter server IP address or name: ")
         clientSocket.connect((serverName,serverPort))
 
-        key = getKey()
-        
-        # Client receives a message and send it to the client
+        # Generate RSA Keys for client
+        pubkey, privkey = generateRSAKeys(username)
+
+        # Client receives server's public key
+        server_pubkey= clientSocket.recv(2048)
+        saveServerPubKey(server_pubkey)
+
+        # Client sends client pubkey to server
+        clientSocket.send(pubkey)
+    
+        # Client receives a message and decrypts it
         encrypted_welcome = clientSocket.recv(2048)
-        decrypted_welcome = decryptMessage(key, encrypted_welcome)
+        decrypted_welcome = decryptMessageRSA(privkey, encrypted_welcome).decode('ascii')
         print(decrypted_welcome)
 
-        name = input()
+        username = input()
 
-        encrypted_name = encryptMessage(key, name)
+        encrypted_name = encryptMessageRSA(server_pubkey, username)
         clientSocket.send(encrypted_name)
 
-        # Password
+        # Password request from server
         encrypted_pass_msg = clientSocket.recv(2048)
-        decrypted_pass_msg = decryptMessage(key, encrypted_pass_msg)
+        decrypted_pass_msg = decryptMessageRSA(privkey, encrypted_pass_msg).decode('ascii')
         print(decrypted_pass_msg)
 
+        # Input password and send to server
         password = input()
-        encrypted_password = encryptMessage(key, password)
+        encrypted_password = encryptMessageRSA(server_pubkey, password)
         clientSocket.send(encrypted_password)
+
+        # recieve sym_key - after this all communication will use symmetric encryption, not RSA
+        encrypted_sym_key = clientSocket.recv(2048)
+        sym_key = decryptMessageRSA(privkey, encrypted_sym_key) # No need to decode here as sym_key is bytes as is
 
         # Start Menu Loop
         while True:
             
             # Receive Menu from server
             encrypted_menu = clientSocket.recv(2048)
-            decrypted_menu = decryptMessage(key, encrypted_menu)
+            decrypted_menu = decryptMessage(sym_key, encrypted_menu)
             print(decrypted_menu)
 
             choice = input()
 
-            encrypted_choice = encryptMessage(key, choice)
+            encrypted_choice = encryptMessage(sym_key, choice)
             clientSocket.send(encrypted_choice)
 
             if choice == '1': # Create and Send Email
 
                 # Enter Email
                 Encrypted_email = clientSocket.recv(2048)
-                decrypted_email = decryptMessage(key, Encrypted_email) 
+                decrypted_email = decryptMessage(sym_key, Encrypted_email) 
                 print(decrypted_email)
 
                 email_input = input()
-                email_send = encryptMessage(key, email_input)
+                email_send = encryptMessage(sym_key, email_input)
                 clientSocket.send(email_send)
 
                 # Enter Title
                 Encrypted_title = clientSocket.recv(2048)
-                decrypted_title = decryptMessage(key, Encrypted_title) 
+                decrypted_title = decryptMessage(sym_key, Encrypted_title) 
                 print(decrypted_title)
 
                 title_input = input()
-                title_send = encryptMessage(key, title_input)
+                title_send = encryptMessage(sym_key, title_input)
                 clientSocket.send(title_send)
 
                 # Load Contents 
                 Encrypted_load = clientSocket.recv(2048)
-                decrypted_load = decryptMessage(key, Encrypted_load) 
+                decrypted_load = decryptMessage(sym_key, Encrypted_load) 
                 print(decrypted_load)
 
                 load_input = input()
-                load_send = encryptMessage(key, load_input)
+                load_send = encryptMessage(sym_key, load_input)
                 clientSocket.send(load_send)
 
                 if load_input.upper().strip() == 'Y':
                     # Path
                     Encrypted_path = clientSocket.recv(2048)
-                    decrypted_path = decryptMessage(key, Encrypted_path) 
+                    decrypted_path = decryptMessage(sym_key, Encrypted_path) 
                     print(decrypted_path)
 
                     path_input = input()
-                    path_send = encryptMessage(key, path_input)
+                    path_send = encryptMessage(sym_key, path_input)
                     clientSocket.send(path_send)
                 
                 else:
                     # Enter Content
                     Encrypted_content = clientSocket.recv(2048)
-                    decrypted_content = decryptMessage(key, Encrypted_content) 
+                    decrypted_content = decryptMessage(sym_key, Encrypted_content) 
                     print(decrypted_content)
 
                     content_input = input()
-                    content_send = encryptMessage(key, content_input)
+                    content_send = encryptMessage(sym_key, content_input)
                     clientSocket.send(content_send)
 
                 # Confirmation Message
                 Encrypted_conf = clientSocket.recv(2048)
-                decrypted_conf = decryptMessage(key, Encrypted_conf)
+                decrypted_conf = decryptMessage(sym_key, Encrypted_conf)
                 print(decrypted_conf)
 
                 # Send ACK So that sever doesnt send menu right away
                 ack_msg = "OK"
-                encrypted_ack = encryptMessage(key, ack_msg)
+                encrypted_ack = encryptMessage(sym_key, ack_msg)
                 clientSocket.send(encrypted_ack)
 
 
             elif choice == '2': # Display inbox list
                 encrypted_prompt = clientSocket.recv(4096)
-                decrypted_prompt = decryptMessage(key, encrypted_prompt)
+                decrypted_prompt = decryptMessage(sym_key, encrypted_prompt)
                 print(decrypted_prompt)
 
                 # Send ACK So that sever doesnt send menu right away
                 ack_msg = "OK"
-                encrypted_ack = encryptMessage(key, ack_msg)
+                encrypted_ack = encryptMessage(sym_key, ack_msg)
                 clientSocket.send(encrypted_ack)
 
             elif choice == '3':
                 # Display the email contents
                 encrypted_prompt = clientSocket.recv(2048)
-                decrypted_prompt = decryptMessage(key, encrypted_prompt)
+                decrypted_prompt = decryptMessage(sym_key, encrypted_prompt)
                 print(decrypted_prompt)
 
                 user_input = input()
 
-                encrypted_input = encryptMessage(key, user_input)
+                encrypted_input = encryptMessage(sym_key, user_input)
                 clientSocket.send(encrypted_input)
 
                 encrypted_content = clientSocket.recv(2048)
-                decrypted_content = decryptMessage(key, encrypted_content)
+                decrypted_content = decryptMessage(sym_key, encrypted_content)
                 print(decrypted_content)
 
                 # Send ACK So that sever doesnt send menu right away
                 ack_msg = "OK"
-                encrypted_ack = encryptMessage(key, ack_msg)
+                encrypted_ack = encryptMessage(sym_key, ack_msg)
                 clientSocket.send(encrypted_ack)
 
             elif choice == '4': # Terminate connection
                 encrypted_goodbye = clientSocket.recv(2048)
-                decrypted_goodbye = decryptMessage(key, encrypted_goodbye)
+                decrypted_goodbye = decryptMessage(sym_key, encrypted_goodbye)
                 print(decrypted_goodbye)
                 clientSocket.close()
                 break
--- server/server.py
+++ server/server.py
@@ -1,10 +1,13 @@
 # Server code Term Project CMPT361
 
 import socket
+import json
 import sys
 import os
 import random
 from Crypto.Cipher import AES
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
 from Crypto.Util.Padding import pad, unpad
 
 # Helper Functions For Encryption/Decryption
@@ -12,56 +15,138 @@ from Crypto.Util.Padding import pad, unpad
 def generateAESKey(client_name):
     # Generate a 256 AES key
     key = os.urandom(32)  # AES-256
-    with open(f'../client/{client_name}/sym_key', 'wb') as f:
-        f.write(key)
+
+    # Create client directory if it doesn't exist
+    os.makedirs(f'client/{client_name}', exist_ok=True)
     return key
 
+#def getKey():
+#    with open('../key', 'rb') as f:
+#        key = f.read()
+#    return key
 
-def getKey():
-    with open('../key', 'rb') as f:
-        key = f.read()
-    return key
 
+# Saves the pubkey of the client for later use, if doesnt already exist
+def saveClientPubKey(client_name, key):
+    if not os.path.exists(f'client/{client_name}/{client_name}_public.pem'):
+        with open(f'client/{client_name}/{client_name}_public.pem', 'wb') as f:
+            f.write(key)
+
+def generateRSAKeys():
+    # Checks if keys exist on server machine
+    if os.path.exists('server_public.pem') and os.path.exists('server_private.pem'):
+        with open('server_public.pem', 'rb') as f:
+            public_key = f.read()
+
+        with open('server_private.pem', 'rb') as f:
+            private_key = f.read()
+        return public_key, private_key
+    
+    else:
+    # Generates RSA key pair and saves to server machine
+        key = RSA.generate(2048)
+        private_key = key.export_key()
+        with open('server_private.pem', 'wb') as f:
+            f.write(private_key)
+
+        public_key = key.publickey().export_key()
+        with open('server_public.pem', 'wb') as f:
+            f.write(public_key)
+        return public_key, private_key
+
+# For sending encrypted messages using client pubkey
+def encryptMessageRSA(public_key, message):
+    pubkey = RSA.import_key(public_key)
+    cipher_rsa_en = PKCS1_OAEP.new(pubkey)
+
+    if isinstance(message, str):  
+        enc_data = cipher_rsa_en.encrypt(message.encode('ascii'))
+    else:
+        enc_data = cipher_rsa_en.encrypt(message)
+    return enc_data
+
+# For receiving encrypted messages using server privkey
+def decryptMessageRSA(private_key, encrypted_message):
+    privkey = RSA.import_key(private_key)
+    cipher_rsa_dec = PKCS1_OAEP.new(privkey)
+    dec_data = cipher_rsa_dec.decrypt(encrypted_message)
+    return dec_data
+
+# Encrypting using AES symmetric key
 def encryptMessage(key, message):
     cipher = AES.new(key, AES.MODE_ECB)
 
     # Bytes then padded
     padded_message = pad(message.encode('ascii'), AES.block_size)
-    encryped_message = cipher.encrypt(padded_message)
-    return encryped_message
+    encrypted_message = cipher.encrypt(padded_message)
+    return encrypted_message
 
+# Decrypting using AES symmetric key
 def decryptMessage(key, encrypted_message):
     cipher = AES.new(key, AES.MODE_ECB)
     decrypted_padded_message = cipher.decrypt(encrypted_message)
     decrypted_message = unpad(decrypted_padded_message, AES.block_size)
     return decrypted_message.decode('ascii')
 
-def client_handeler(connectionSocket, addr, key):   
+def client_handeler(connectionSocket, addr):   
     # This handles the entire exam for a single client connection
+
+    # Load all client names and passwords from file
+
+    # Prior to symkey generation, use RSA to securely exchange username/password
+    # Encrypt using client pubkey when sending to client, decrypt using server privkey when receiving from client
+
+    with open('user_pass.json', 'r') as f:
+        user_pass_dict = json.load(f)
 
     client_name = ""
     try:
 
-        #Server send a message to the client
+        #Server sends pubkey to client
+        pubkey, privkey = generateRSAKeys()
+        connectionSocket.send(pubkey)
+
+        #Server recieves encrypted client pubkey
+        client_pubkey = connectionSocket.recv(2048)
+
         welcome_msg = "Enter your name: "
-        encryped_message = encryptMessage(key, welcome_msg)
-        connectionSocket.send(encryped_message)
+        encrypted_message = encryptMessageRSA(client_pubkey, welcome_msg)
+        connectionSocket.send(encrypted_message)
                 
         # Server receives client user name
         encrypted_name = connectionSocket.recv(2048)
-        NameStr = decryptMessage(key, encrypted_name).strip()
+        NameStr = decryptMessageRSA(privkey, encrypted_name).decode('ascii').strip()
 
         # Server asks for password
         password_msg = f"Enter your password: "
-        encrypted_password_msg = encryptMessage(key, password_msg)
+        encrypted_password_msg = encryptMessageRSA(client_pubkey, password_msg)
         connectionSocket.send(encrypted_password_msg)
 
         # Server receives password
         encrypted_password = connectionSocket.recv(2048)
-        PasswordStr = decryptMessage(key, encrypted_password).strip()
+        PasswordStr = decryptMessageRSA(privkey, encrypted_password).decode('ascii').strip()
         
         print(f"Encrypted message recv: {encrypted_name}")
         print(f"encrypted message decrypted: {NameStr}")
+        # Authentication for client, ensuring they exist in user_pass.json
+        if NameStr in user_pass_dict and user_pass_dict[NameStr] == PasswordStr:
+            auth_msg = f"Connection Accepted and Symmetric Key Generated for client: {NameStr}"
+            print(auth_msg)
+            sym_key = generateAESKey(NameStr)
+            saveClientPubKey(NameStr, client_pubkey)
+            
+            # send symmetric key to client
+            encrypted_sym_key = encryptMessageRSA(client_pubkey, sym_key)
+            connectionSocket.send(encrypted_sym_key)
+        else:
+            connectionSocket.send("Invalid username or password")
+            auth_msg = f"The received client information: {NameStr} is invalid (Connection Terminated)."
+
+            connectionSocket.send(auth_msg.encode('ascii'))
+            connectionSocket.close()    
+            
+            # Terminate child process
+            os._exit(0)
 
         # Start Menu Loop
         while True:
@@ -74,12 +159,12 @@ def client_handeler(connectionSocket, addr, key):
                 "Choice: "
                 )
 
-            encrypted_menu = encryptMessage(key, menu)
+            encrypted_menu = encryptMessage(sym_key, menu)
             connectionSocket.send(encrypted_menu)
 
             # Wait for client choice
             encrypted_choice = connectionSocket.recv(2048)
-            choice = decryptMessage(key, encrypted_choice).strip()
+            choice = decryptMessage(sym_key, encrypted_choice).strip()
 
             if choice == '1':
                 # Create and send an email
@@ -94,34 +179,30 @@ def client_handeler(connectionSocket, addr, key):
                 # [message contents with a maximum length of 1000000 characters]
 
                 Enter_email_prompt = "Enter emails (separated by ;): "
-                encrypted_prompt = encryptMessage(key, Enter_email_prompt)
+                encrypted_prompt = encryptMessage(sym_key, Enter_email_prompt)
                 connectionSocket.send(encrypted_prompt)
 
                 user_input = connectionSocket.recv(2048)
-                email_list = decryptMessage(key, user_input).strip().split(';')
-
+                email_list = decryptMessage(sym_key, user_input).strip().split(';')
                 Enter_title_prompt = "Enter title: "
-                encrypted_prompt = encryptMessage(key, Enter_title_prompt)
+                encrypted_prompt = encryptMessage(sym_key, Enter_title_prompt)
                 connectionSocket.send(encrypted_prompt)
 
                 user_input = connectionSocket.recv(2048)
-                email_title = decryptMessage(key, user_input).strip()
-
+                email_title = decryptMessage(sym_key, user_input).strip()
                 load_content_prompt = "Would you like to load contents from a file? (Y/N): "
-                encrypted_prompt = encryptMessage(key, load_content_prompt)
+                encrypted_prompt = encryptMessage(sym_key, load_content_prompt)
                 connectionSocket.send(encrypted_prompt)
 
                 user_input = connectionSocket.recv(2048)
-                load_choice = decryptMessage(key, user_input).strip().upper()
-
+                load_choice = decryptMessage(sym_key, user_input).strip().upper()
                 if load_choice == 'Y':
                     file_prompt = "Enter file path: "
-                    encrypted_prompt = encryptMessage(key, file_prompt)
+                    encrypted_prompt = encryptMessage(sym_key, file_prompt)
                     connectionSocket.send(encrypted_prompt)
 
                     user_input = connectionSocket.recv(2048)
-                    file_path = decryptMessage(key, user_input).strip()
-
+                    file_path = decryptMessage(sym_key, user_input).strip()
                     try:
                         with open(file_path, 'r') as f:
                             email_content = f.read()
@@ -130,12 +211,11 @@ def client_handeler(connectionSocket, addr, key):
 
                 else:
                     Enter_content_prompt = "Enter content:"
-                    encrypted_prompt = encryptMessage(key, Enter_content_prompt)
+                    encrypted_prompt = encryptMessage(sym_key, Enter_content_prompt)
                     connectionSocket.send(encrypted_prompt)
 
                     user_input = connectionSocket.recv(2048)
-                    email_content = decryptMessage(key, user_input).strip()
-
+                    email_content = decryptMessage(sym_key, user_input).strip()
                 # Format full email
 
                 from datetime import datetime
@@ -157,7 +237,7 @@ def client_handeler(connectionSocket, addr, key):
                 for recipient in email_list:
                     recipient = recipient.strip()
                     if recipient:
-                        recipient_dir = f"../client/{recipient}"
+                        recipient_dir = f"client/{recipient}"
                         if not os.path.exists(recipient_dir):
                             os.makedirs(recipient_dir)
 
@@ -171,7 +251,7 @@ def client_handeler(connectionSocket, addr, key):
                 
                 # Send Confirmation Message
                 Confirmation_prompt = "The message is sent to the server."
-                encrypted_prompt = encryptMessage(key, Confirmation_prompt)
+                encrypted_prompt = encryptMessage(sym_key, Confirmation_prompt)
                 connectionSocket.send(encrypted_prompt)
 
                 # Client ACK so that menu doesnt send right away
@@ -186,7 +266,7 @@ def client_handeler(connectionSocket, addr, key):
                 # 1     client2 2022-07-21 19:29:35.768508 Test2
                 # 2     client1 2022-07-21 19:29:42.118132 Test
 
-                client_directory = f"../client/{NameStr}"
+                client_directory = f"client/{NameStr}"
                 
                 if not os.path.exists(client_directory):
                     full_summary = "Inbox is empty (No directory found)."
@@ -208,7 +288,7 @@ def client_handeler(connectionSocket, addr, key):
                 summary_content = "\n".join(email_summaries)
                 full_summary = summary_line + summary_header + summary_content
                 
-                encrypted_summary = encryptMessage(key, full_summary)
+                encrypted_summary = encryptMessage(sym_key, full_summary)
                 connectionSocket.send(encrypted_summary)
 
                 # Client ACK so that menu doesnt send right away
@@ -217,23 +297,23 @@ def client_handeler(connectionSocket, addr, key):
             elif choice == '3':
                 # Display the email contents
                 enter_index_prompt = "Enter the index of the email to display:\n"
-                encrypted_prompt = encryptMessage(key, enter_index_prompt)
+                encrypted_prompt = encryptMessage(sym_key, enter_index_prompt)
                 connectionSocket.send(encrypted_prompt)
 
                 user_input = connectionSocket.recv(2048)
-                email_index = int(decryptMessage(key, user_input).strip())
-                client_directory = f"../client/{NameStr}"
+                email_index = int(decryptMessage(sym_key, user_input).strip())
+                client_directory = f"client/{NameStr}"
                 email_files = [f for f in os.listdir(client_directory) if os.path.isfile(os.path.join(client_directory, f))]
                 if 1 <= email_index <= len(email_files):
                     email_file = email_files[email_index - 1]
                     with open(os.path.join(client_directory, email_file), 'r') as f:
                         email_content = f.read()
                     email_line = "----------------------------------------------"
-                    encrypted_email_content = encryptMessage(key, email_line + email_content)
+                    encrypted_email_content = encryptMessage(sym_key, email_line + email_content)
                     connectionSocket.send(encrypted_email_content)
                 else:
                     error_msg = "Invalid email index."
-                    encrypted_error_msg = encryptMessage(key, error_msg)
+                    encrypted_error_msg = encryptMessage(sym_key, error_msg)
                     connectionSocket.send(encrypted_error_msg)
 
                 connectionSocket.recv(2048)
@@ -241,7 +321,7 @@ def client_handeler(connectionSocket, addr, key):
             elif choice == '4':
                 # Terminate connection
                 goodbye_msg = "Terminated."
-                encrypted_goodbye = encryptMessage(key, goodbye_msg)
+                encrypted_goodbye = encryptMessage(sym_key, goodbye_msg)
                 connectionSocket.send(encrypted_goodbye)
                 break
 
@@ -273,12 +353,18 @@ def server():
         sys.exit(1)        
         
     print('The server is ready to accept connections')
-        
+    
+    # Call pub/priv key creation function here
+    #
+    generateRSAKeys()
+
     #The server can only have one connection in its queue waiting for acceptance
     serverSocket.listen(5)
     
-    # Need to do SHA Key Here
-    key = getKey()
+    
+
+    # get symmetric key in client_handler
+    # key = getKey()
 
     while 1:
         try:
@@ -292,7 +378,7 @@ def server():
                 
                 serverSocket.close() 
                 
-                client_handeler(connectionSocket, addr, key)
+                client_handeler(connectionSocket, addr)
                 
                 connectionSocket.close()
                 
@@ -311,6 +397,4 @@ def server():
             serverSocket.close() 
             sys.exit(0)
             
-        
-#-------
 server()
